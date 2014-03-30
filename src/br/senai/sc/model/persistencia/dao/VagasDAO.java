697fd948b92d86224776ef0b2027ebe0f81f675a
GUSTAVO <GUSTAVO@GUSTAVO-PC>
2014-03-30 19:50:46 -0300

    Feitas as alterações necessárias, APENAS na classe VagasDAO, o que tem de erro ae, ja tava

--- src/br/senai/sc/model/persistencia/dao/VagasDAO.java
+++ src/br/senai/sc/model/persistencia/dao/VagasDAO.java
@@ -1,99 +1,29 @@
-
 package br.senai.sc.model.persistencia.dao;
 
 import br.senai.sc.model.negocio.Vagas;
 import java.util.List;
 
+/**
+ * Interface responsavél pela declaração dos métodos relacionados a persistencia
+ * de classe vagas.
+ *
+ * @author Bruna Zakrzeski
+ * @since 1.0 05/11/2013
+ * @param vag
+ * @return
+ */
+public interface VagasDAO extends DAO<Vagas, Integer> {
+
+
     /**
-     * Interface responsavél pela declaração dos métodos relacionados
-     * a persistencia de classe vagas.
+     * Método responsável pela listar pesquisa de treinamento
+     *
      * @author Bruna Zakrzeski
-     * @since 1.0  05/11/2013
-     * @param vag
-     * @return 
+     * @version 1.0 09/09/13
+     * @param treina
+     * @return
      */
-
-
-public interface VagasDAO {
-    
-    
-    
-    
-    
-    
-   /**
-   * Método responsável pela inserção de vagas
-   * @author Bruna Zakrzeski
-   * @version 1.0 09/09/13
-   * @param vaga
-   * @return 
-   */
-    
-    boolean insert(Vagas vaga);
-    
-    
-    
-    
-   /**
-   * Método responsável pela alteração de vagas
-   * @author Bruna Zakrzeski
-   * @version 1.0 09/09/13
-   * @param vaga
-   * @return 
-   */
-    
-    boolean update(Vagas vaga);
-    
-    
-    
-    
-   /**
-   * Método responsável pela remoção de vagas
-   * @author Bruna Zakrzeski
-   * @version 1.0 09/09/13
-   * @param vaga
-   * @return 
-   */
-    
-    boolean delete(int codigo);
-    
-    
-    
-    
-   /**
-   * Método responsável pela listagem de vagas todos 
-   * @author Bruna Zakrzeski
-   * @version 1.0 09/09/13
-   * @param vaga
-   * @return 
-   */
-    
-    List<Vagas> listAll();
-    
-    
-    
-    
-   /**
-   * Método responsável pela listar pesquisa de treinamento
-   * @author Bruna Zakrzeski
-   * @version 1.0 09/09/13
-   * @param treina
-   * @return 
-   */
     List<Vagas> listPesquisa(String texto);
-    
-    
-    
-    
-    
-      /**
-   * Método responsável pela lista por id do treinamento
-   * @author Bruna Zakrzeski
-   * @version 1.0 09/09/13
-   * @param treina
-   * @return 
-   */
-    Vagas listById(int id);
-    
+
     
 }
