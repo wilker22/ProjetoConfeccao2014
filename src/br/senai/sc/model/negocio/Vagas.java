bf0e95bc1eb58c2689603ae3cebb520b26b52bff
GUSTAVO <GUSTAVO@GUSTAVO-PC>
2014-03-30 19:49:59 -0300

    Feitas as alterações necessárias, APENAS na classe Vagas, o que tem de erro ae, ja tava

--- src/br/senai/sc/model/negocio/Vagas.java
+++ src/br/senai/sc/model/negocio/Vagas.java
@@ -1,30 +1,38 @@
-
 package br.senai.sc.model.negocio;
 
 import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
 
-    /**
-     * Classe responsavél por guardar os métodos
-     * get e set dp vagas
-     * de acordo com o banco de dados
-     * @author Bruna Zakrzeski
-     * @since 1.0  05/11/2013
-     * @param vag
-     * @return 
-     */ 
-
+/**
+ * Classe responsavél por guardar os métodos get e set dp vagas de acordo com o
+ * banco de dados
+ *
+ * @author Bruna Zakrzeski
+ * @since 1.0 05/11/2013
+ * @param vag
+ * @return
+ */
 public class Vagas {
-    
-    //VARIAVEIS declaradas
-    public int codigo;
-    public String nome;
-    public int qtVaga;
-    public String preRequisito;
-    public String funcao;
-    public double faixaSalarial;
-    
 
- 
+    //VARIAVEIS declaradas
+    @Id
+    @GeneratedValue
+    public int codigo;
+    @Column(nullable = true)
+    public String nome;
+    @Column(nullable = true)
+    public int qtVaga;
+    @Column(nullable = true)
+    @Lob
+    public String preRequisito;
+    @Column(nullable = true)
+    public String funcao;
+    @Column(nullable = true)
+    public double faixaSalarial;
+
     public Vagas() {
     }
 
@@ -77,22 +85,20 @@ public class Vagas {
         this.faixaSalarial = faixaSalarial;
     }
     //Get e Set finalizado
-    
-    
+
     //toString declarado
-    public String toString(){
-        return "Código: " + this.getCodigo() +
-               "\nNome: " + this.getNome() +
-               "\nQuantidade de vagas: " + this.getQtVaga()+ 
-               "\nPré Requisitos: " + this.getPreRequisito() +
-               "\nFunção: " + this.getFuncao() +
-               "\nFaixa Salarial: " + this.getfaixaSalarial();
-        
-        
-        
+    public String toString() {
+        return "Código: " + this.getCodigo()
+                + "\nNome: " + this.getNome()
+                + "\nQuantidade de vagas: " + this.getQtVaga()
+                + "\nPré Requisitos: " + this.getPreRequisito()
+                + "\nFunção: " + this.getFuncao()
+                + "\nFaixa Salarial: " + this.getfaixaSalarial();
+
+
+
 
     }
-
 
     //Iniciando o equals
     @Override
@@ -107,7 +113,7 @@ public class Vagas {
         if (this.codigo != other.codigo) {
             return false;
         }
-   
+
         if (!Objects.equals(this.nome, other.nome)) {
             return false;
         }
@@ -124,8 +130,7 @@ public class Vagas {
             return false;
         }
         return true;
-        
+
         //Finalizando o equals
     }
-    
 }
